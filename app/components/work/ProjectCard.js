ecab6faebd205d454cba30b4f19f2fa58d7f8821
7mjake <7m.jake@gmail.com>
2025-08-28 16:41:39 -0400

    increased image size and added gradient overlay

--- app/components/work/ProjectCard.js
+++ app/components/work/ProjectCard.js
@@ -7,13 +7,16 @@ export default function ProjectCard({ project }) {
             "flex flex-row outline outline-3 outline-primary p-4]",
             project.id % 2 === 0 && "flex-row-reverse"
         )}>
-            <Image
-                src={project.image}
-                alt={project.title}
-                width={200}
-                height={150}
-                className="w-1/2 object-cover"
-            />
+            <div className="relative w-1/2">
+                <Image
+                    src={project.image}
+                    alt={project.title}
+                    width={500}
+                    height={500}
+                    className="w-full object-cover drop-shadow-2xl"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-background/50 from-1% via-transparent to-99% to-background/50"></div>
+            </div>
             <div className="flex flex-col w-1/2 p-8 justify-center gap-6">
                 <h3 className="text-6xl font-climate-crisis text-primary">{project.title}</h3>
                 <p className="text-lg">{project.description}</p>
