3ea7c0fc747b731bd0eeaf0364da806c9b2df76c
7mjake <7m.jake@gmail.com>
2025-08-28 15:35:47 -0400

    Added footer content

--- app/components/global/footer.js
+++ app/components/global/footer.js
@@ -1,12 +1,30 @@
+import Container from './Container'
+import JmLogo from './svgs/jm-logo'
+import { ArrowUpRight } from 'feather-icons-react'
+
 export default function Footer() {
     return (
-        <footer className="flex flex-col justify-between items-center">
-            <p>© 2025 Jake Martin</p>
-            <p>
-                <a href="https://github.com/jake-martin">GitHub</a>
-                <a href="https://www.linkedin.com/in/jake-martin-1234567890/">LinkedIn</a>
-                <a href="https://www.instagram.com/jake.martin/">Instagram</a>
-            </p>
-        </footer>
+        <Container>
+            <footer className="flex flex-row justify-between items-start mb-6">
+                <div className='flex flex-1 flex-col gap-2'>
+                    <JmLogo className='fill-primary' />
+                    <p>Made with love and spite</p>
+                    <p>© 2025 Jake Martin</p>
+                </div>
+                <div className='flex flex-1 flex-col gap-2'>
+                    <h3 className='text-lg font-bold'>Contact</h3>
+                    <a href="https://www.linkedin.com/in/jakemartin-design//" className='flex items-center gap-2'>LinkedIn <ArrowUpRight className='size-4' /></a>
+                    <a href="https://github.com/7mjake" className='flex items-center gap-2'>GitHub <ArrowUpRight className='size-4' /></a>
+                    <a href="mailto:jake@jake.com" className='flex items-center gap-2'>me@jakemartin.design</a>
+                </div>
+                <div className='flex flex-1 flex-col gap-2'>
+                    <h3 className='text-lg font-bold'>Navigation</h3>
+                    <a href="/">Work</a>
+                    <a href="/fun">Fun</a>
+                    <a href="/about">About</a>
+                    <a href="/resume">Resume</a>
+                </div>
+            </footer>
+        </Container>
     )
 }
