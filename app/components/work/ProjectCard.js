ede910938ca6b4dd35328e040546a1df76b1e164
7mjake <7m.jake@gmail.com>
2025-08-28 16:18:02 -0400

    added project covers

--- app/components/work/ProjectCard.js
+++ app/components/work/ProjectCard.js
@@ -7,16 +7,16 @@ export default function ProjectCard({ project }) {
             "flex flex-row outline outline-3 outline-primary p-4]",
             project.id % 2 === 0 && "flex-row-reverse"
         )}>
-            <Image 
-                src={project.image} 
-                alt={project.title} 
+            <Image
+                src={project.image}
+                alt={project.title}
                 width={200}
                 height={150}
                 className="w-1/2 object-cover"
             />
-            <div className="flex flex-col w-1/2 p-8 justify-center gap-4">
+            <div className="flex flex-col w-1/2 p-8 justify-center gap-6">
                 <h3 className="text-6xl font-climate-crisis text-primary">{project.title}</h3>
-                <p className="text-gray-500">{project.description}</p>
+                <p className="text-lg">{project.description}</p>
                 <div className="flex flex-row gap-[3px]">
                     {project.tags.map((tag) => (
                         <span className="text-primary font-medium outline outline-3 outline-primary px-2" key={tag}>{tag}</span>
