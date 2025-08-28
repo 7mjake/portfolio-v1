c96fffc55d6542f7defdbfbdd9d1f799c1cb64ad
7mjake <7m.jake@gmail.com>
2025-08-28 16:10:29 -0400

    Styled footer headers

--- app/components/global/footer.js
+++ app/components/global/footer.js
@@ -5,20 +5,20 @@ import { ArrowUpRight } from 'feather-icons-react'
 export default function Footer() {
     return (
         <Container>
-            <footer className="flex flex-row justify-between items-start mb-6">
+            <footer className="flex flex-row justify-between items-start mb-24">
                 <div className='flex flex-1 flex-col gap-5 items-start'>
-                    <JmLogo className='h-7 py-1' />
+                    <JmLogo className='h-7 py-1 text-primary' />
                     <p>Made with love and spite</p>
                     <p>© 2025 Jake Martin</p>
                 </div>
                 <div className='flex flex-1 flex-col gap-5'>
-                    <h3 className='text-lg font-bold'>Contact</h3>
+                    <h3 className='text-lg font-bold text-primary'>Contact</h3>
                     <a href="https://www.linkedin.com/in/jakemartin-design//" className='flex items-center gap-2'>LinkedIn <ArrowUpRight className='size-4' /></a>
                     <a href="https://github.com/7mjake" className='flex items-center gap-2'>GitHub <ArrowUpRight className='size-4' /></a>
                     <a href="mailto:jake@jake.com" className='flex items-center gap-2'>me@jakemartin.design</a>
                 </div>
                 <div className='flex flex-1 flex-col gap-5'>
-                    <h3 className='text-lg font-bold'>Navigation</h3>
+                    <h3 className='text-lg font-bold text-primary'>Navigation</h3>
                     <a href="/">Work</a>
                     <a href="/fun">Fun</a>
                     <a href="/about">About</a>
