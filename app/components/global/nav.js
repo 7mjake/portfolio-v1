0133447358f538f90052bef83ea8ab211048df60
7mjake <7m.jake@gmail.com>
2025-08-28 14:44:05 -0400

    Styled navbar and added logo

--- app/components/global/nav.js
+++ app/components/global/nav.js
@@ -1,18 +1,24 @@
 import Link from 'next/link'
 import Container from './Container'
+import { Sun, Moon } from 'feather-icons-react'
+import Image from 'next/image'
+import JmLogo from './svgs/jm-logo'
 
 export default function Nav() {
     return (
         <Container>
-        <div className="flex justify-between m-4">
-            <Link href="/">Logo</Link>
-            <nav>
-                <Link href="/">Work</Link>
-                <Link href="/fun">Fun</Link>
-                <Link href="/about">About</Link>
-                <Link href="/resume">Resume</Link>
-            </nav>
-        </div>
+            <div className="flex justify-between pt-6 text-primary">
+                <Link href="/">
+                    <JmLogo className='fill-primary' />
+                </Link>
+                <nav className='flex gap-6'>
+                    <Link href="/">Work</Link>
+                    <Link href="/fun">Fun</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/resume">Resume</Link>
+                    <button><Sun /></button>
+                </nav>
+            </div>
         </Container>
     )
 }
---
+++ app/components/global/svgs/jm-logo.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+const JmLogo = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={32}
+        height={20}
+        fill="none"
+        {...props}
+    >
+        <path
+            fill="currentColor"
+            d="M5.715 20C2.572 20 0 17.429 0 14.286V8.572c3.143 0 5.715 2.571 5.715 5.714V20ZM8.57 20V0h5.715v14.286c0 3.142-2.572 5.714-5.715 5.714ZM22.855 20H17.14V0c3.143 0 5.715 2.571 5.715 5.714V20ZM31.429 20h-5.715V0c3.143 0 5.715 2.571 5.715 5.714V20Z"
+        />
+    </svg>
+)
+export default JmLogo
