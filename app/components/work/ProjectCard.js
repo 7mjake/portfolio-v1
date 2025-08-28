ba620a11e87de9482272bc38163713a291c7b6da
7mjake <7m.jake@gmail.com>
2025-08-28 09:24:22 -0400

    Styled project tags and added spacing

--- app/components/work/ProjectCard.js
+++ app/components/work/ProjectCard.js
@@ -14,12 +14,12 @@ export default function ProjectCard({ project }) {
                 height={150}
                 className="w-1/2 object-cover"
             />
-            <div className="flex flex-col w-1/2 p-8">
+            <div className="flex flex-col w-1/2 p-8 justify-center gap-4">
                 <h3 className="text-6xl font-climate-crisis text-primary">{project.title}</h3>
                 <p className="text-gray-500">{project.description}</p>
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-[3px]">
                     {project.tags.map((tag) => (
-                        <span key={tag}>{tag}</span>
+                        <span className="text-primary font-medium outline outline-3 outline-primary px-2" key={tag}>{tag}</span>
                     ))}
                 </div>
             </div>
