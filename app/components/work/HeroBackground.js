df0d149bc4771176cac1c43b6d645484decffc5f
7mjake <7m.jake@gmail.com>
2025-08-29 09:57:46 -0400

    Optimized unicorn animation

--- app/components/work/HeroBackground.js
+++ app/components/work/HeroBackground.js
@@ -2,12 +2,14 @@ import UnicornHero from './UnicornHero'
 
 export default function HeroBackground() {
     return (
-        <div className='absolute flex flex-col top-0 left-0 z-10 w-full opacity-0 mix-blend-multiply pointer-events-none animate-[fadeIn_2s_ease-in-out_0.5s_forwards]'>
+        <div className='absolute flex flex-col top-0 left-0 z-10 w-full mix-blend-multiply pointer-events-none animate-[fadeIn_2s_ease-in-out_0.5s_forwards]'>
             <div className='z-0'>
                 <UnicornHero />
             </div>
-            <div className='absolute bg-gradient-to-t from-white h-full w-full' />
+            {/* <div className='absolute bg-gradient-to-t from-white h-full w-full' /> */}
 
+            <div className='absolute bottom-0 bg-gradient-to-t from-white h-[100px] w-full' />
+            <div className='absolute bottom-0 bg-gradient-to-t from-white h-[200px] w-full' />
             <div className='absolute bottom-0 bg-gradient-to-t from-white h-[300px] w-full' />
         </div>
     )
--- app/components/work/UnicornHero.js
+++ app/components/work/UnicornHero.js
@@ -2,8 +2,20 @@ import UnicornScene from "unicornstudio-react/next"
 
 export default function UnicornHero() {
   return (
-    <div className="w-full h-screen">
-      <UnicornScene projectId="aWqHZNM0AaLhK9O2gexn" />
+    <div className="w-full">
+      <UnicornScene
+        projectId="aWqHZNM0AaLhK9O2gexn?update=1.0.1"
+        width="100vw"
+        height="100vh"
+        scale={1}
+        dpi={0.5}
+        fps={30}
+        altText="Animated background scene"
+        ariaLabel="Animated background scene"
+        className="opacity-30"
+        lazyLoad={true}
+        production={true}
+      />
     </div>
   )
 }
