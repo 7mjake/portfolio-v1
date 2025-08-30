88bc3d3b8f918537abfacecce14ce328c44e6fff
7mjake <7m.jake@gmail.com>
2025-08-29 22:06:35 -0400

    Sticky and styled nav

--- app/components/global/Container.js
+++ app/components/global/Container.js
@@ -2,9 +2,9 @@ export default function Container({ children, fullWidth = false }) {
   if (fullWidth) {
     return <div>{children}</div>;
   }
-  
+
   return (
-    <div className="flex flex-col w-full max-w-5xl mx-auto">
+    <div className="relative flex flex-col w-full max-w-5xl mx-auto">
       {children}
     </div>
   );
--- app/components/global/nav.js
+++ app/components/global/nav.js
@@ -5,19 +5,21 @@ import ThemeToggle from './ThemeToggle'
 
 export default function Nav() {
     return (
-        <Container>
-            <div className="flex justify-between pt-6 text-primary items-center">
-                <Link href="/">
-                    <JmLogo className='fill-primary h-7 py-1' />
-                </Link>
-                <nav className='flex gap-6 items-center'>
-                    <Link href="/">Work</Link>
-                    <Link href="/fun">Fun</Link>
-                    <Link href="/about">About</Link>
-                    <Link href="/resume">Resume</Link>
-                    <ThemeToggle />
-                </nav>
-            </div>
-        </Container>
+        <div className={'sticky top-0 z-10 bg-gradient-to-b from-background to-background/50 backdrop-blur-sm'}>
+            <Container>
+                <div className="flex justify-between py-6 text-primary items-center border-b-[3px] border-primary -mx-[3px]">
+                    <Link href="/">
+                        <JmLogo className='fill-primary h-7 py-1' />
+                    </Link>
+                    <nav className='flex gap-6 items-center'>
+                        <Link href="/">Work</Link>
+                        <Link href="/fun">Fun</Link>
+                        <Link href="/about">About</Link>
+                        <Link href="/resume">Resume</Link>
+                        <ThemeToggle />
+                    </nav>
+                </div>
+            </Container>
+        </div>
     )
 }
