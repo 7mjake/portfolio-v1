b08d9c661042f1eed762fc26194413a9e3e3e902
7mjake <7m.jake@gmail.com>
2025-09-03 22:04:37 -0400

    Added dev origin config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const JakesiPhone = '192.168.68.66'
+const JakesMacBookPro = '192.168.68.62'
+
 const nextConfig = {
   // experimental.appDir is no longer needed in Next.js 15
   // The App Router is now stable and enabled by default
+  allowedDevOrigins: [
+    'local-origin.dev',
+    '*.local-origin.dev',
+    JakesMacBookPro,
+    JakesiPhone,
+  ],
 }
 
 module.exports = nextConfig
