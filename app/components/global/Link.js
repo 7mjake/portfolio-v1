a6970db9cffac98a983dc0de2fedfcb14f85f717
7mjake <7m.jake@gmail.com>
2025-09-04 21:31:59 -0400

    fixed link props and transition

--- app/components/global/Link.js
+++ app/components/global/Link.js
@@ -1,14 +1,19 @@
 import NextLink from 'next/link'
 import clsx from 'clsx'
 
-export default function Link({ className, children, ...props }) {
+export default function Link({
+  className,
+  children,
+  button = false,
+  ...props
+}) {
   return (
     <NextLink
       className={clsx(
         className,
-        'w-fit rounded-lg hover:scale-105',
-        !props.button &&
-          'hover:bg-primary/10 hover:outline-primary/10 -translate-x-2 px-2 transition-transform hover:outline-4'
+        'w-fit rounded-lg transition-transform hover:scale-105',
+        !button &&
+          'hover:bg-primary/10 hover:outline-primary/10 -translate-x-2 px-2 hover:outline-4'
       )}
       {...props}
     >
